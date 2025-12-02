264d46ec69a05cb5cbf6692355c20785b852f335
chixchix55 <jsegovich55@gmail.com>
2025-12-02 09:38:34 -0400

    Cambio en el export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // necesario para poder exportar sin procesar imágenes
   },
+  basePath: '',           // IMPORTANTE
+  assetPrefix: '.',       // pez clave: usa rutas relativas
+  trailingSlash: true     // para que genere index.html en cada carpeta
 };
 
 export default nextConfig;
