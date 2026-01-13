910d8e4ca21606596434ea4f7b416999fdd5e1b1
Iagovisk <pedritoeusebio@hotmail.com>
2026-01-13 14:22:45 -0400

    Cambios en favicon y open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,32 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import FooterWrapper from "../components/footerwrapper";
-import NavbarWrapper from "../components/navbarwrapper";  // <- Importa el wrapper
+import NavbarWrapper from "../components/navbarwrapper"; // <- Importa el wrapper
 import ClientLayout from "./ClientLayout"; // <- Importa el ClientLayout
 
 export const metadata: Metadata = {
-  title: 'ATEX',
-  description: 'A modern real estate experience',
+	title: "ATEX",
+	description: "A modern real estate experience",
+	icons: {
+		icon: "/img/atex_logo.png",
+	},
+	openGraph: {
+		images: "/img/Atex Full.webp",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <NavbarWrapper /> {/* Ahora el Navbar es client-only */}
-        <ClientLayout>{children}</ClientLayout>
-        <FooterWrapper />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<NavbarWrapper /> {/* Ahora el Navbar es client-only */}
+				<ClientLayout>{children}</ClientLayout>
+				<FooterWrapper />
+			</body>
+		</html>
+	);
 }
